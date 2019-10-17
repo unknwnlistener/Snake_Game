c0f4cfa1d15d7545d058b9df63bbc6dafc8d46cc
Javier Yzaguirre <phoenixfiremind77@gmail.com>
2019-10-17 15:03:10 -0500

    Changes made for Hacktoberfest 2019 PR

--- snake.js
+++ snake.js
@@ -1,28 +1,26 @@
 function Snake() {
   this.x = 0;
   this.y = 0;
-  this.xSpeed = scale * 3;
+  this.xSpeed = scale * 1 ;
   this.ySpeed = 0;
   this.total = 0;
   this.tail = [];
 
   this.draw = function() {
     ctx.fillStyle = "#FFFFFF";
-    for (let i=0; i<this.tail.length; i++) {
-      ctx.fillRect(this.tail[i].x,
-        this.tail[i].y, scale, scale);
+    for (let i=0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
     }
 
     ctx.fillRect(this.x, this.y, scale, scale);
   }
 
   this.update = function() {
-    for (let i=0; i<this.tail.length - 1; i++) {
-      this.tail[i] = this.tail[i+1];
+    for (let i=0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
     }
 
-    this.tail[this.total - 1] =
-      { x: this.x, y: this.y };
+    this.tail[this.total - 1] = { x: this.x, y: this.y };
 
     this.x += this.xSpeed;
     this.y += this.ySpeed;
@@ -48,31 +46,30 @@ function Snake() {
     switch(direction) {
       case 'Up':
         this.xSpeed = 0;
-        this.ySpeed = -scale * (this.total + 1);
+        this.ySpeed = -scale * 1 /*(this.total + 1)*/;
         break;
       case 'Down':
         this.xSpeed = 0;
-        this.ySpeed = scale * (this.total + 1);
+        this.ySpeed = scale * 1 /*(this.total + 1)*/;
         break;
       case 'Left':
-        this.xSpeed = -scale * (this.total + 1);
+        this.xSpeed = -scale * 1 /*(this.total + 1)*/;
         this.ySpeed = 0;
         break;
       case 'Right':
-        this.xSpeed = scale * (this.total + 1);
+        this.xSpeed = scale * 1 /*(this.total + 1)*/;
         this.ySpeed = 0;
         break;
     }
   }
 
   this.eat = function(fruit) {
-    if (this.x === fruit.x &&
-      this.y === fruit.y) {
+    if (this.x === fruit.x && this.y === fruit.y) {
       this.total++;
-      if(this.ySpeed == 0)
-        this.xSpeed *= 1.5;
-      else
-        this.ySpeed *= 1.5;
+      // if(this.ySpeed == 0)
+      //   this.xSpeed *= 1.5;
+      // else
+      //   this.ySpeed *= 1.5;
       return true;
     }
     return false;
@@ -80,8 +77,7 @@ function Snake() {
 
   this.checkCollision = function() {
     for (var i=0; i<this.tail.length; i++) {
-      if (this.x === this.tail[i].x &&
-        this.y === this.tail[i].y) {
+      if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
         this.total = 0;
         this.tail = [];
       }
