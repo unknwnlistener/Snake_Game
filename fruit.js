ac408a23d3a7a336e5e27dadacf5e930184c8c7a
Muhammad Haseeb <haseeb.940@gmail.com>
2020-10-01 14:37:57 +0500

    Improves fruit and score UI

--- fruit.js
+++ fruit.js
@@ -1,16 +1,19 @@
 function Fruit() {
   this.x;
   this.y;
+  this.fruitNumber;
 
   this.pickLocation = function() {
     this.x = (Math.floor(Math.random() *
       columns - 1) + 1) * scale;
     this.y = (Math.floor(Math.random() *
       rows - 1) + 1) * scale;
+    this.fruitNumber = Math.floor(Math.random() * 2);
   }
 
   this.draw = function() {
-    ctx.fillStyle = "#4FF0A8";
-    ctx.fillRect(this.x, this.y, scale, scale)
+    var img = new Image();
+    img.src = `./assets/${this.fruitNumber}.png`;
+    ctx.drawImage(img, this.x, this.y, scale, scale);
   }
 }
